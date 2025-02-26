5f18fe8e23256d66435f37a598c3ed56796fd4bb
Vic-Hndz-PC <victor.hernandez06@goumh.umh.es>
2025-02-26 17:55:17 +0100

    Revert "feat: chakra ui initial add"
    This reverts commit 91e00ff4ef727bb03ba255b13372ddbe5fd42f52.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,10 @@
-import "./index.css";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "@/components/ui/provider";
-import App from "./App.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
