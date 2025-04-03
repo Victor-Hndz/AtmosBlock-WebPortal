2e28380d582d286eef189e8a3b793dd0f02d9f05
Vic-Hndz-PC <victor.hernandez06@goumh.umh.es>
2025-04-03 19:17:57 +0200

    feat: user entity mod correctly use the db

--- backend/nestjs/src/users/entities/user.entity.ts
+++ backend/nestjs/src/users/entities/user.entity.ts
@@ -6,8 +6,10 @@ import {
   UpdateDateColumn,
   BeforeInsert,
   BeforeUpdate,
+  OneToMany,
 } from "typeorm";
 import * as bcrypt from "bcrypt";
+import { Request } from "../../requests/entities/request.entity";
 
 export enum UserRole {
   ADMIN = "admin",
@@ -35,14 +37,17 @@ export class User {
   })
   role: UserRole;
 
-  @Column({ default: true })
+  @Column({ name: "is_active", default: true })
   isActive: boolean;
 
-  @CreateDateColumn()
+  @CreateDateColumn({ name: "created_at" })
   createdAt: Date;
 
-  @UpdateDateColumn()
+  @UpdateDateColumn({ name: "updated_at" })
   updatedAt: Date;
+
+  @OneToMany(() => Request, request => request.user)
+  requests: Request[];
 
   @BeforeInsert()
   @BeforeUpdate()
