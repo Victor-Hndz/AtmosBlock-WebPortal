d471e929d0218405b1fefc854a8c777a669d409c
Víctor Hernández <vic.hernandezs08@gmail.com>
2025-05-01 18:37:19 +0200

    refactor: standardize database environment variable naming from DB_DATABASE to DB_NAME

--- backend/nestjs/src/config/config.module.ts
+++ backend/nestjs/src/config/config.module.ts
@@ -15,7 +15,7 @@ import * as Joi from "joi";
         DB_PORT: Joi.number().required(),
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
-        DB_DATABASE: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
         // JWT
         JWT_SECRET: Joi.string().required(),
         JWT_EXPIRATION: Joi.string().required(),
