b893b18b6fee872a848707f5897f3b153ede3b3c
Víctor Hernández <vic.hernandezs08@gmail.com>
2025-05-03 11:14:16 +0200

    feat: Enhance JwtStrategy to include user validation and error handling

--- backend/nestjs/src/auth/strategies/jwt.strategy.ts
+++ backend/nestjs/src/auth/strategies/jwt.strategy.ts
@@ -1,11 +1,15 @@
 import { ExtractJwt, Strategy } from "passport-jwt";
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ConfigService } from "@nestjs/config";
+import { UsersService } from "@/users/services/users.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly usersService: UsersService
+  ) {
     const secretKey = configService.get<string>("JWT_SECRET");
     if (!secretKey) {
       throw new Error("JWT_SECRET is not defined in environment variables");
@@ -19,6 +23,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
+    const user = await this.usersService.findOne(payload.sub);
+
+    if (!user) {
+      throw new UnauthorizedException("User not found");
+    }
+
     return {
       id: payload.sub,
       email: payload.email,
