c9a9bc747cfe89075e1626c9b3e6c01e418e6d2c
scolab_server <victor.hernandez06@goumh.umh.es>
2025-05-23 17:11:06 +0200

    feat: integrate ResultsService and ResultsConsumer with GeneratedFilesModule

--- backend/nestjs/src/generatedFiles/generatedFiles.module.ts
+++ backend/nestjs/src/generatedFiles/generatedFiles.module.ts
@@ -4,17 +4,26 @@ import { GeneratedFilesService } from "./services/generatedFiles.service";
 import { GeneratedFilesEntity } from "./persistence/entities/generatedFiles.entity";
 import { TypeOrmGeneratedFilesRepository } from "./persistence/repositories/typeorm-generatedFiles.repository";
 import { MinioService } from "@/minio/services/minio.service";
+import { ResultsService } from "./services/results.service";
+import { ResultsController } from "./controllers/results.controller";
+import { ResultsConsumerController } from "./controllers/results-consumer.controller";
+import { ResultsConsumer } from "./messaging/results.consumer";
+import { RabbitMQModule } from "@/shared/messaging/rabbitmq.module";
+import { ProgressModule } from "@/progress/progress.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GeneratedFilesEntity])],
+  imports: [TypeOrmModule.forFeature([GeneratedFilesEntity]), RabbitMQModule, ProgressModule],
+  controllers: [ResultsController, ResultsConsumerController],
   providers: [
     MinioService,
     GeneratedFilesService,
+    ResultsService,
+    ResultsConsumer,
     {
       provide: "IGeneratedFilesRepository",
       useClass: TypeOrmGeneratedFilesRepository,
     },
   ],
-  exports: [GeneratedFilesService],
+  exports: [GeneratedFilesService, ResultsService],
 })
 export class GeneratedFilesModule {}
